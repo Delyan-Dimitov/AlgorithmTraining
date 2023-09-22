168a791fb9051173f355fb90d0d8de09b81a93ff
Guneto <gunexon@gmail.com>
2023-09-22 17:52:14 +0300

    Bubble Sort

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,24 @@
+﻿namespace Sorting.BubbleSort
+{
+    internal static class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            var length = array.Length;
+            while (length > 1)
+            {
+                for (int i = 0; i < length -1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        var temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+
+                    }
+                }
+                length--;
+            }
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,4 @@
+﻿using Sorting.BubbleSort;
+
+int[] array = new int[] {1,3,5,2,4,9,8,7,6,10};
+BubbleSort.Sort(array);
